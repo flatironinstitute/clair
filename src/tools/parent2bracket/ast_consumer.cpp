69fa5f70da59cd55f76576b5c7a04f392ed2569b
Olivier Parcollet <oparcollet@flatironinstitute.org>
2024-08-04 07:53:48 -0400

    more tests

--- src/tools/parent2bracket/ast_consumer.cpp
+++ src/tools/parent2bracket/ast_consumer.cpp
@@ -18,7 +18,7 @@ void ast_consumer::HandleTranslationUnit(clang::ASTContext &ctx) {
   // All call expression of an operator() of an class with name nda::.*
   mf.addMatcher(callExpr(unless(isExpansionInSystemHeader()),                              //
                          callee(cxxMethodDecl(ofClass(matchesName("nda::.*"))).bind("m")), //
-                         hasArgument(0, declRefExpr().bind("B")))
+                         hasArgument(0, declRefExpr().bind("caller")))
                    .bind("call"),
                 &ma);
   // NB the isExpansionInSystemHeader removes everything included with -isystem

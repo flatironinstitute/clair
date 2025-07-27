f6b9f0dec22f19abd630e1e5e067445b8993985c
Olivier Parcollet <oparcollet@flatironinstitute.org>
2025-07-27 15:44:55 -0400

    Remove generator

--- src/utility/generator.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#if __has_include(<generator>) // Check if <generator> exists
-#include <generator>
-using std::generator;
-#else
-#include "cppcoro/generator.hpp"
-using cppcoro::generator;
-#endif

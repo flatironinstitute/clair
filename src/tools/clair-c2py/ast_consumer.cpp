a1292e1fb0a320d21c80f0fa1d0cef5176ce81b3
Olivier Parcollet <oparcollet@flatironinstitute.org>
2025-12-22 15:32:47 +0100

    Refactor AST matching to use two MatchFinder instances
    Minor cleaning

--- src/tools/clair-c2py/ast_consumer.cpp
+++ src/tools/clair-c2py/ast_consumer.cpp
@@ -116,24 +116,23 @@ void ast_consumer::HandleTranslationUnit(clang::ASTContext &ctx) {
   // ------- Match the AST in two passes
   // Pass 1: Match classes and enums first, so all classes are collected
   // before we process functions (which may reference these classes)
-  MatchFinder mf_1;
+  // Pass 2: Match functions after that all classes are known
+  MatchFinder mf1, mf2;
   matcher<mtch::Cls> ma_cls{worker};
   matcher<mtch::Enum> ma_enum{worker};
   matcher<mtch::ModuleClsWrap> ma_using{worker};
 
-  mf_1.addMatcher(add_excludes(call_cls).bind("class"), &ma_cls);
-  mf_1.addMatcher(add_excludes(call_enum).bind("en"), &ma_enum);
-  mf_1.addMatcher(namespaceDecl(isExpansionInMainFile(), hasName("c2py_module"), //
+  mf1.addMatcher(add_excludes(call_cls).bind("class"), &ma_cls);
+  mf1.addMatcher(add_excludes(call_enum).bind("en"), &ma_enum);
+  mf2.addMatcher(add_excludes(call_fun).bind("func"), &ma_f);
+  mf1.addMatcher(namespaceDecl(isExpansionInMainFile(), hasName("c2py_module"), //
                                 forEach(typeAliasDecl().bind("decl"))),
                   &ma_using);
-  mf_1.matchAST(ctx);
+  mf1.matchAST(ctx);
   if (ctx.getDiagnostics().hasErrorOccurred()) return;
 
-  // Pass 2: Match functions now that all classes are known
-  MatchFinder mf_2;
   matcher<mtch::Fnt> ma_f{worker};
-  mf_2.addMatcher(add_excludes(call_fun).bind("func"), &ma_f);
-  mf_2.matchAST(ctx);
+  mf2.matchAST(ctx);
   if (ctx.getDiagnostics().hasErrorOccurred()) return;
 
   // -------- done ----------

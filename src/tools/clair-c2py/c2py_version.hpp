95cd3429aec9cd845e4ccc7f53e9c0ca43c2e438
Olivier Parcollet <oparcollet@flatironinstitute.org>
2026-08-03 17:10:38 -0400

    Bump c2py cxx code version to 1.0

--- src/tools/clair-c2py/c2py_version.hpp
+++ src/tools/clair-c2py/c2py_version.hpp
@@ -2,5 +2,5 @@
 
 // The exact value of the version of c2py for which the wrappers
 // are generated.
-static constexpr auto c2py_version_major = "0";
-static constexpr auto c2py_version_minor = "1";
+static constexpr auto c2py_version_major = "1";
+static constexpr auto c2py_version_minor = "0";
